d51d388db44f6482397bc4ba7121694eaaba676d
Bhanu2240 <bhanuaitireddy224@gmail.com>
2026-05-25 14:57:32 +0530

    Fix production backend URL

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : "http://18.208.197.41:5001/api",
-
+  baseURL: "http://18.208.197.41:5001/api",
   withCredentials: true,
 });
