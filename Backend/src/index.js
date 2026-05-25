f5c46377a6819f3b8fefcf59b5f5c7ee2694cda6
Bhanu2240 <bhanuaitireddy224@gmail.com>
2026-05-25 14:52:43 +0530

    Add EC2 frontend URL to CORS

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -17,7 +17,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "http://18.208.197.41:5173"
+    ],
     credentials: true,
   })
 );
