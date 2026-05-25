8604ddb3014607805c63c63cf5e357ae729a5a72
Bhanu2240 <bhanuaitireddy224@gmail.com>
2026-05-25 14:46:10 +0530

    Update production API URL

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "http://18.208.197.41:5001/api",
+
   withCredentials: true,
 });
